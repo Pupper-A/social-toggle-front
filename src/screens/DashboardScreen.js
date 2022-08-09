726cf4bf009582d92278935c76448cd30d3ffd37
hmphne <36847007+hmphne@users.noreply.github.com>
2022-08-09 15:40:30 +0430

    Add files via upload

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -7,114 +7,17 @@ import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 import { sendToggle } from '../actions/toggleActions';
 import axios from "axios";
 import CustomizedSwitches, { BasicSwitch } from '../components/MuiSwitch';
-import Profile from '../components/Profile';
-import Username from '../components/Username';
+import ProfileCard from '../components/ProfileCard';
+import { Typography } from '@mui/material';
 
 function DashboardScreen() {
-//     const [toggle, setToggle] = useState("")
-//     const [time, setTime] = useState("")
-//     const [message, setMessage] = useState("")
-//     const [limit, setLimit] = useState("")
 
-//     const history = useNavigate();
-    
-//     let redirect = ""
-    
-//     const dispatch = useDispatch()
-    
-//     const userToggle = useSelector(state => state.toggle)
-
-//     const {error, loading} = userToggle
-
-//     let token = null
-    
-//     useEffect(() => {
-//         const userInfo = localStorage.getItem("userInfo")
-
-//         if(userInfo){
-//             token = "Bearer " + JSON.parse(userInfo).token
-//         }else{
-//             redirect = "/"
-//             history(redirect)
-//         }
-
-//         async function getToggleInfo(){
-//             const config = {
-//                 headers: {
-//                     "Authorization": token
-//                 }
-//             }
-            
-//             await axios.get("/api/users/toggle/", config).then((response) => {
-//                 if(response.data){
-//                     a = new Date(response.data.toggled_time)
-//                     setToggle(response.data.is_toggled)
-                    
-//                 }else if(!localStorage.getItem("userInfo")){
-//                     redirect = "/"
-//                     history(redirect)
-//                 }else{
-//                     redirect = "/new-user"
-//                     history(redirect)
-//                 }
-                
-//             })
-//         }
-        
-//         let a = null
-
-//         getToggleInfo()
-
-//         if(time<10){
-//             setLimit(false)
-//             setToggle(toggle)
-
-//         }
-
-//         setInterval(() => {
-//             let b = new Date()
-//             let def = Math.round((b-a)/60000)
-//             setTime(String(def))
-
-//         }, 1000);
-
-//     }, [limit])
-
-//     const toggleHandler = (checked) => {
-        
-//             setTime(0)
-//             var bool = 0
-//             setToggle(checked)
-//             if(toggle === ""){
-//                 bool = 1
-//             }else if(toggle == true){
-//                 bool = 0
-//             }else{
-//                 bool = 1
-//             }
-//             const userId = JSON.parse(localStorage.getItem("userInfo")).id
-//             dispatch(sendToggle(userId, bool))
-//             window.location.reload(false)
-
-        
-//   }
 
   return (
-      <Container>
-        {/* {loading && <Loader />}
-        {message && <Alert variant='danger'>{message}</Alert>}
-        {error && <Alert variant='danger'>{error}</Alert>} */}
-        
-
+      <Container>     
         <Row>
-            <Col className="d-flex justify-content-center">
-                <Profile />
-            </Col>
-        </Row>
-        <Row>
-            <Col className="my-1 py-1 text-center">
-                
-                <Username />
+            <Col className="my-4 py-4 text-center justify-content-center">     
+                <ProfileCard />
             </Col>
         </Row>
 
@@ -122,10 +25,8 @@ function DashboardScreen() {
         
         <Col className="my-5 py-5 text-center">
             <CustomizedSwitches
-           
-
              />
-             <p>What is your MOOD?</p>
+             <Typography variant='body' sx={{color:'#1E8582', fontWeight:500}}>Toggle your mood</Typography>
         </Col>
         
         </Row>
