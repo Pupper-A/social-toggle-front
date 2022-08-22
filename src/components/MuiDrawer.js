fe7bea1e0c2c5200dd0aecea7612b2c18e8eab3c
hmphne <36847007+hmphne@users.noreply.github.com>
2022-08-22 16:23:36 +0430

    Update MuiDrawer.js

--- src/components/MuiDrawer.js
+++ src/components/MuiDrawer.js
@@ -1,4 +1,4 @@
-import { Drawer, Box, Typography, IconButton } from '@mui/material';
+import { Drawer, Box, Typography, IconButton, SvgIcon } from '@mui/material';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
@@ -13,6 +13,10 @@ import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import {useState} from 'react';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import ListSubheader from '@mui/material/ListSubheader';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PersonIcon from '@mui/icons-material/Person';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {ReactComponent as PupperIcon} from '../PupperIcon.svg';
 
 
 export const MuiDrawer = () => {
@@ -27,40 +31,73 @@ export const MuiDrawer = () => {
 
     const list = (anchor) => (
         <Box
-          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 320 }}
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 320,
+          color:'#1B1B1B',
+         }}
           role="presentation"
           onClick={()=> setIsDrawerOpen(false)}
           onKeyDown={()=> setIsDrawerOpen(false)}
         >
-          <List sx={{paddingBottom:57}}
+          <List sx={{paddingBottom:'56vh'}}
           subheader={
             <ListSubheader style={boxStyle} component="div" id="nested-list-subheader">
                 Contact Us
             </ListSubheader>
           } >
-            {['Pupper', 'Linkedin', 'Email'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={PupperIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary='Pupper' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LinkedInIcon
+                    style={{fill:'#1E8582'}}
+                   /> 
+                </ListItemIcon>
+                <ListItemText primary='LinkedIn' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <MailIcon
+                   style={{fill:'#1E8582'}}
+                   />
+                </ListItemIcon>
+                <ListItemText primary='Email' />
+              </ListItemButton>
+            </ListItem>
+
           </List>
           <Divider />
           <List>
-            {['Sign out', 'Delete Account'].map((text, index) => (
-              <ListItem key={text} disablePadding>
+            <ListItem disablePadding>
                 <ListItemButton>
                   <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <PersonIcon
+                   style={{fill:'#1B1B1B'}}
+                   />
                   </ListItemIcon>
-                  <ListItemText primary={text} />
+                  <ListItemText primary='Sign out' />
                 </ListItemButton>
-              </ListItem>
-            ))}
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DeleteIcon
+                   style={{fill:'#D8295E'}}
+                   />
+                </ListItemIcon>
+                <ListItemText primary='Delete Account'
+                style={{color:'#D8295E'}}
+                 />
+              </ListItemButton>
+            </ListItem>
           </List>
         </Box>
       );
